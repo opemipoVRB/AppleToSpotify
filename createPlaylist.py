dedb671b909be8adafe944cf174fbd221b3ed91b
opemipoVRB <opemipodurodola@gmail.com>
2020-07-19 13:23:36 +0100

    Initial commit

---
+++ createPlaylist.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+↓↓...........................................................................↓↓
+↓↓..........................↓↓↓↓↓↓↓↓↓↓↓↓↓....................................↓↓
+↓↓.......................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.................................↓↓
+↓↓.....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓...............................↓↓
+↓↓....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.↓↓...............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓...↓↓..............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.↓↓...↓↓↓.............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..............................↓↓
+↓↓....................↓↓↓↓↓↓↓↓↓↓↓↓↓.....↓↓↓↓↓↓↓↓↓............................↓↓
+↓↓......................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..↓↓↓↓↓↓↓............................↓↓
+↓↓...................................↓↓↓.....................................↓↓
+↓↓.................↓↓................↓↓↓↓ ↓↓↓↓↓↓↓........↓...................↓↓
+↓↓...............↓↓↓↓↓↓..............↓↓↓↓↓↓↓↓↓↓↓↓↓...↓↓↓↓↓↓..................↓↓
+↓↓............↓↓↓↓..↓↓↓↓↓.........................↓↓↓↓↓↓↓↓↓..................↓↓
+↓↓............↓↓↓↓...↓↓↓↓↓↓↓....................↓↓↓↓↓↓.↓↓.↓↓.................↓↓
+↓↓...............↓↓↓↓↓↓↓↓↓↓↓↓↓↓............↓↓↓↓↓↓↓↓..........................↓↓
+↓↓.........................↓↓↓↓↓↓↓↓↓...↓↓↓↓↓↓↓...............................↓↓
+↓↓..............................↓↓↓↓↓↓↓↓↓↓...................................↓↓
+↓↓..........................↓↓↓↓↓....↓↓↓↓↓↓↓↓↓...............................↓↓
+↓↓............↓↓.↓↓↓↓↓↓↓↓↓↓↓↓↓............↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..................↓↓
+↓↓............↓↓.↓↓..↓↓↓↓.....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓.................↓↓
+↓↓..............↓↓↓↓↓↓............................↓↓.↓↓↓↓↓↓↓.................↓↓
+↓↓..................                                   ......................↓↓
+↓↓.................. ↑↑↑  ↑↑↑  ↑↑↑↑↑↑↑        ↑↑↑↑↑↑↑ .......................↓↓
+↓↓.................. ↑↑↑  ↑↑↑  ↑↑↑   ↑↑↑↑     ↑↑↑   ↑↑↑↑.....................↓↓
+↓↓.................. ↑↑↨  ↑↑↑  ↑↑↨   ↨↑↑      ↑↑↨   ↨↑↑......................↓↓
+↓↓.................. ↨↑↨  ↑↨↑  ↨↑↨   ↨↑↨      ↨↑↨   ↨↑↨......................↓↓
+↓↓.................. ↑↨↑  ↨↑↨  ↨↨↑↨↑↨↨↑↑↨     ↨↨↑↨↑↨↨↑↑↨.....................↓↓
+↓↓.................. ↨↑↨  ↨↨↨  ↨↨↨      ↨↨↨   ↨↨↨     ↨↨↨....................↓↓
+↓↓.................. :↨:  ↨↨:  ↨↨:      :↨↨   ↨↨:     :::....................↓↓
+↓↓................... ::↨↨:↨   :↨:      :↨:   :↨:     :::....................↓↓
+↓↓.................... ::::    :::      :::   :::     :::....................↓↓
+↓↓...................... :      :        :      :::::::  ....................↓↓
+↓↓...........................................................................↓↓
+↓↓←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←↓↓
+↓↓→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→↓↓
+↓↓      create_playlist.py  Created by  Durodola Opemipo 2020                 ↓↓
+↓↓            Personal Email : <opemipodurodola@gmail.com>                   ↓↓
+↓↓                 Telephone Number: +2348182104309                          ↓↓
+↓↓→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→↓↓
+↓↓←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←↓↓
+
+"""
+import os
+
+import requests
+import spotipy
+import spotipy.util as util
+from fuzzywuzzy import fuzz
+
+from settings import env_
+
+clientID = env_["SPOTIPY_CLIENT_ID"]
+clientSecret = env_["SPOTIPY_CLIENT_SECRET"]
+username = env_["SPOTIFY_USERNAME"]
+scope = 'playlist-modify-public'
+
+username = username
+token = util.prompt_for_user_token(
+    username,
+    scope,
+    client_id=clientID,
+    client_secret=clientSecret,
+    redirect_uri='http://localhost:8080/callback'
+)
+
+sp = spotipy.Spotify(auth=token)
+
+
+def create_playlist(name, description):
+    sp.user_playlist_create(username, name=name, description=description)
+    # api-endpoint
+    print("Playlist Created.")
+    return "Done"
+
+
+def get_tracks(data):
+    titles = data["tracks"]
+    track_ids = []
+    for i in range(len(data)):
+        results = sp.search(q=f"{data['tracks'][i]} {data['artists'][i]} ", limit=5,
+                            type='track')  # get 5 responses since first isn't always accurate
+        if results['tracks']['total'] == 0:  # if track isn't on spotify as queried, go to next track
+            continue
+        else:
+            for j in range(len(results['tracks']['items'])):
+                if not (not (90 < fuzz.partial_ratio(results['tracks']['items'][j]['artists'][0]['name'],
+                                                     data['artists'][i])) or not (
+                        fuzz.partial_ratio(results['tracks']['items'][j]['name'],
+                                           data['tracks'][
+                                               i]) > 90)):  # Improve accuracy by matching on artist and title
+                    track_ids.append(results['tracks']['items'][j]['id'])  # append track id
+                    break  # don't want repeats of a sample ex: different versions
+                else:
+                    continue
+
+    annotation_track_ids = []
+
+    for title in titles:
+        results = sp.search(q=f"{title}", type='track')
+        print(results)
+        if results['tracks']['total'] == 0:  # Skip track if not found
+            continue
+        else:
+            annotation_track_ids.append(results['tracks']['items'][0]['id'])
+    track_ids = track_ids + annotation_track_ids
+    print("Got TrackIDs")
+    print("Found ", len(titles), " tracks")
+    return track_ids
+
+
+def get_playlist_id(playlist_name):
+    playlist_id = ''
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:  # iterate through playlists I follow
+        if playlist['name'] == playlist_name:  # filter for newly created playlist
+            playlist_id = playlist['id']
+    return playlist_id
+
+
+def add_tracks_to_playlist(playlist, tracks):
+    sp.user_playlist_add_tracks(username, playlist, tracks)
+    print("Done")
